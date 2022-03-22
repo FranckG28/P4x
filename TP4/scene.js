66e048d5e802009fa95f88747d2b871759f061a9
GUTMANN FRANCK <fgutmann@etu.unistra.fr>
2022-03-22 10:21:44 +0100

    Nettoyage de scene.js

--- TP4/scene.js
+++ TP4/scene.js
@@ -21,7 +21,8 @@ var scene, camera, renderer;
 const lightColor = "#FFFFFF";
 
 /* Chargement des classes */
-const objtool = new OBJTool();
+const objTool = new OBJTool();
+const textureLoader = new THREE.TextureLoader();
 
 /* SCENE */
 scene = new THREE.Scene();
@@ -31,9 +32,16 @@ camera = new THREE.PerspectiveCamera(fov, W / H, 0.1, 1000);
 camera.position.set(0, 10, 30);
 camera.lookAt(scene.position);
 
+/* LUMIERES */
+const light = new THREE.DirectionalLight( 0xffffff, 1 );
+light.position.x = 3;
+light.position.y = 5;
+light.position.z = 4;
+light.intensity = 1.0;
+scene.add( light );
+
 /* SOL */
-const floorLoader = new THREE.TextureLoader();
-floorLoader.load(
+textureLoader.load(
         // resource URL
         'moon.png',
 
@@ -49,7 +57,7 @@ floorLoader.load(
                 mat.side = THREE.DoubleSide;
                 var floor = new THREE.Mesh(geo, mat);
                 floor.position.set(0, -0.01, 0)
-                floor.rotateX(objtool.makeAngle(90))
+                floor.rotateX(objTool.makeAngle(90))
                 scene.add(floor)
         },
 
@@ -61,128 +69,6 @@ floorLoader.load(
                 console.error( 'An error happened.' );
         }
 );
-
-
-/* LUMIERES */
-const light = new THREE.DirectionalLight( 0xffffff, 1 );
-light.position.x = 3;
-light.position.y = 5;
-light.position.z = 4;
-light.intensity = 1.0;
-scene.add( light );
-
-
-/* SHADERS */
-
-/********** LAMBERT *********** */
-// Vertex shader
-var lambertVertexShader = `
-        uniform vec3 lightPos;
-        varying vec3 vNormal;
-        varying vec3 vPosition;
-
-        void main()
-        {
-                vec4 worldPos = modelMatrix * vec4(position, 1.0);
-                gl_Position = projectionMatrix * viewMatrix * worldPos;
-
-                vNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
-                vPosition = worldPos.xyz;
-        }`
-
-
-// Pixel shader
-var lambertFragmentShader = `
-        varying vec3 vNormal;
-        varying vec3 vPosition;
-        uniform vec3 rgb;
-        uniform vec3 lightPos;
-        uniform vec3 lightColor;
-        uniform float lightIntensity;
-
-        void main()
-        {
-                vec3 color = (rgb*lightIntensity*lightColor) * dot(normalize(vNormal), normalize(lightPos - vPosition));
-                gl_FragColor = vec4(color, 1.0);
-        }`
-
-/************ TOON ************/
-
-// Pixel shader
-var toonFragmentShader = `
-        varying vec3 vNormal;
-        varying vec3 vPosition;
-        uniform vec3 rgb;
-        uniform vec3 lightPos;
-        uniform vec3 lightColor;
-        uniform float lightIntensity;
-
-        void main()
-        {
-
-                float scal = dot(normalize(vNormal), normalize(lightPos - vPosition));
-
-                if (scal < 0.33) {
-                        scal = 0.33;
-                } else if (scal < 0.66) {
-                        scal = 0.66;
-                } else {
-                        scal = 1.0;
-                }
-
-                vec3 color = (rgb*lightIntensity*lightColor) * scal;
-                gl_FragColor = vec4(color, 1.0);
-        }`
-
-
-/*** PHONG ***/
-
-var phongFragmentShader = `
-        varying vec3 vNormal;
-        varying vec3 vPosition;
-
-        uniform vec3 rgb;
-        uniform vec3 cameraPos;
-        uniform vec3 lightPos;
-        uniform vec3 lightColor;
-        uniform float lightIntensity;
-
-        void main()
-        {
-
-                vec3 lightDirection = normalize(lightPos - vPosition);
-                float scal = dot(lightDirection, normalize(vNormal));
-                vec3 toCamera = normalize(cameraPos - vPosition);
-
-                float visibility = dot(toCamera, lightDirection);
-                float specular = pow(dot(toCamera, normalize(vNormal)), (lightIntensity*20.0));
-
-                vec3 color = (rgb*lightIntensity*lightColor);
-                vec3 diffuse = color * scal;
-                vec3 vecSpecular = color * specular * visibility;
-
-                gl_FragColor = vec4(diffuse + vecSpecular, 1.0);
-        }`
-
-
-// *** UNIFORMS ***/
-var myRGBUniform = { type: "v3", value: new THREE.Vector3() };
-var myLightPosUniform = { type: "v3", value: light.position};
-var myLightIntensityUniform = { type: "float", value: light.intensity};
-var myLightColorUniform = { type: "v3", value: light.color};
-var myCameraPosUniforms = { type: "v3", value: camera.position};
-
-// on associe la déclaration type/conteneur au nom de la variable uniform "rgb"
-var myUniforms = { rgb : myRGBUniform, lightPos: myLightPosUniform, lightIntensity: myLightIntensityUniform, lightColor: myLightColorUniform, cameraPos: myCameraPosUniforms};
-  
-/*** Création des shaders ***/
-
-const lamberShaderMaterial = new THREE.ShaderMaterial({ vertexShader: lambertVertexShader, fragmentShader: lambertFragmentShader, uniforms: myUniforms });
-
-const toonShaderMaterial = new THREE.ShaderMaterial({ vertexShader: lambertVertexShader, fragmentShader: toonFragmentShader, uniforms: myUniforms });
-
-const phongShaderMaterial = new THREE.ShaderMaterial({ vertexShader: lambertVertexShader, fragmentShader: phongFragmentShader, uniforms: myUniforms });
-
 
 /* CIEL */
 
